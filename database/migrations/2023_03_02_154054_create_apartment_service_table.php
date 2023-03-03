fb1307cbec56505a6cbb9d525fc270794a92a33f
tsukigahara <satsuki.tksm@gmail.com>
2023-03-03 14:08:47 +0100

    delete timestamp columns

--- database/migrations/2023_03_02_154054_create_apartment_service_table.php
+++ database/migrations/2023_03_02_154054_create_apartment_service_table.php
@@ -12,8 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('apartment_service', function (Blueprint $table) {
-            $table->    id();
-            $table->    timestamps();
+            $table->id();
         });
     }
 
