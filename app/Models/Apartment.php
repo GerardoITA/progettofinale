e2d373948fa7738e2baeace41b2dce74eba82cb9
GerardoITA <gerardo.giusti@tiscali.it>
2023-03-03 10:25:18 +0100

    Creato model apartment

---
+++ app/Models/Apartment.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Apartment extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'title',
+        'rooms',
+        'password',
+    	'rooms',
+        'beds',	
+        'bathrooms',	
+        'square_meters',	
+        'address',	
+        'latitude',
+        'longitude',	
+        'main_image',	
+        'visible',	
+        'price',	
+        'description',        
+        	
+    ];
+    
+}
