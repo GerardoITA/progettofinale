0cd92276567025172f6f90ba065f24f2aaed30a7
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-03-06 11:12:23 +0100

    Creo rotta store

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::post('/profile', [ProfileController::class, 'store'])->name('profile.store');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
